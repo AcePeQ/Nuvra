6b9679c9eb8b1ee872883677a9788658a6c4b3b4
AcePeQ <maciej.nojszewski22@gmail.com>
2025-11-08 14:29:10 +0100

    feat: add 3 new svg icons, create boilerplate for search component

--- frontend/src/shared/ui/search/Search.tsx
+++ frontend/src/shared/ui/search/Search.tsx
@@ -5,7 +5,7 @@ import XIcon from "/src/assets/images/icons/XIcon.svg";
 function Search() {
   return (
     <form className={styles.form}>
-      <button className={styles.button}>
+      <button className={`${styles.button} ${styles.button__search}`}>
         <img
           className={styles.icon}
           src="/src/assets/images/icons/SearchIcon.svg"
@@ -17,7 +17,7 @@ function Search() {
         type="text"
         placeholder="Search for products..."
       />
-      <button className={styles.button}>
+      <button className={`${styles.button} ${styles.button__clear}`}>
         <img className={styles.icon} src={XIcon} alt="Clear icon" />
       </button>
     </form>
