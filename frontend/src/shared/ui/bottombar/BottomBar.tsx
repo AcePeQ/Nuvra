b9500aa3d70bc710bf748af6d4c8a5903473d70b
AcePeQ <maciej.nojszewski22@gmail.com>
2025-11-08 14:17:24 +0100

    feat: create separation in Header on Left and Right side

--- frontend/src/shared/ui/bottombar/BottomBar.tsx
+++ frontend/src/shared/ui/bottombar/BottomBar.tsx
@@ -1,12 +1,19 @@
+import { s } from "framer-motion/client";
 import Logo from "../logo/Logo";
 import MainNav from "../mainNav/MainNav";
+import Search from "../search/Search";
 import styles from "./BottomBar.module.css";
 
 function BottomBar() {
   return (
     <div className={`container-padding ${styles.bottombar}`}>
-      <Logo />
-      <MainNav />
+      <div className={styles.bottombar__left}>
+        <Logo />
+        <MainNav />
+      </div>
+      <div className={styles.bottombar__right}>
+        <Search />
+      </div>
     </div>
   );
 }
