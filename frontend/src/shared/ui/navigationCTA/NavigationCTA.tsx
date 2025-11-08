9408dd6699b53bfc78cfdc579b3c62744595278f
AcePeQ <maciej.nojszewski22@gmail.com>
2025-11-08 17:43:58 +0100

    feat: create search component with styles, create component for storing navigation CTA buttons

---
+++ frontend/src/shared/ui/navigationCTA/NavigationCTA.tsx
@@ -0,0 +1,7 @@
+import styles from "./NavigationCTA.module.css";
+
+function NavigationCTA() {
+  return <div className={styles.navigationCTA}>NavigationCTA</div>;
+}
+
+export default NavigationCTA;
