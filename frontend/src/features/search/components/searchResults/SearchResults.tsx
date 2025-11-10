20f41793038043df84092ae3d7417c7c5b8948b7
AcePeQ <maciej.nojszewski22@gmail.com>
2025-11-10 14:00:49 +0100

    feat: search result height scroll styles

--- frontend/src/features/search/components/searchResults/SearchResults.tsx
+++ frontend/src/features/search/components/searchResults/SearchResults.tsx
@@ -30,6 +30,58 @@ function SearchResults() {
             </div>
           </Link>
         </li>
+
+        <li className={styles.searchResults__item}>
+          <Link className={styles.searchResults__link} to="/products/1">
+            <img
+              src="/src/assets/images/placeholders/searchPlaceholder.png"
+              className={styles.searchResults__image}
+              alt=""
+            />
+            <div className={styles.searchResults__content}>
+              <h3 className={styles.searchResults__title}>
+                One life graphic t-shirt
+              </h3>
+              <div className={styles.rating}>Rating</div>
+              <p className={styles.searchResults__description}>
+                This graphic t-shirt which is perfect for any occasion. Crafted
+                from a soft and breathable fabric, it offers superior comfort
+                and style.
+              </p>
+              <div className={styles.searchResults__pricing}>
+                <p className={styles.searchResults__price_current}>$260</p>
+                <p className={styles.searchResults__price_original}>$300</p>
+                <span className={styles.searchResults__discount}>-40%</span>
+              </div>
+            </div>
+          </Link>
+        </li>
+
+        <li className={styles.searchResults__item}>
+          <Link className={styles.searchResults__link} to="/products/1">
+            <img
+              src="/src/assets/images/placeholders/searchPlaceholder.png"
+              className={styles.searchResults__image}
+              alt=""
+            />
+            <div className={styles.searchResults__content}>
+              <h3 className={styles.searchResults__title}>
+                One life graphic t-shirt
+              </h3>
+              <div className={styles.rating}>Rating</div>
+              <p className={styles.searchResults__description}>
+                This graphic t-shirt which is perfect for any occasion. Crafted
+                from a soft and breathable fabric, it offers superior comfort
+                and style.
+              </p>
+              <div className={styles.searchResults__pricing}>
+                <p className={styles.searchResults__price_current}>$260</p>
+                <p className={styles.searchResults__price_original}>$300</p>
+                <span className={styles.searchResults__discount}>-40%</span>
+              </div>
+            </div>
+          </Link>
+        </li>
       </ul>
     </div>
   );
