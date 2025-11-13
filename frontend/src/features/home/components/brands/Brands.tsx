40f74b885a3870d78b499698d5086ce3ffba7bed
AcePeQ <maciej.nojszewski22@gmail.com>
2025-11-13 19:22:35 +0100

    feat: add brands images

--- frontend/src/features/home/components/brands/Brands.tsx
+++ frontend/src/features/home/components/brands/Brands.tsx
@@ -1,9 +1,48 @@
 import styles from "./Brands.module.css";
+import VersaceBrand from "/src/assets/images/brands/versace.png";
+import GucciBrand from "/src/assets/images/brands/gucci.png";
+import CalvinBrand from "/src/assets/images/brands/calvin.png";
+import ZaraBrand from "/src/assets/images/brands/zara.png";
+import PradaBrand from "/src/assets/images/brands/prada.png";
+
+const BRANDS_ITEMS = [
+  {
+    id: 1,
+    img: VersaceBrand,
+    alt: "Versace Brand",
+  },
+  {
+    id: 2,
+    img: ZaraBrand,
+    alt: "Zara brand",
+  },
+  {
+    id: 3,
+    img: GucciBrand,
+    alt: "Gucci brand",
+  },
+  {
+    id: 4,
+    img: PradaBrand,
+    alt: "Prada brand",
+  },
+  {
+    id: 5,
+    img: CalvinBrand,
+    alt: "Calvin Klein brand",
+  },
+];
 
 function Brands() {
   return (
     <section className={`container container-padding ${styles.brands_section}`}>
-      <ul className={styles.brands}></ul>
+      <ul className={styles.brands}>
+        {BRANDS_ITEMS.map(({ id, img, alt }) => (
+          <li className={styles.brands__item} key={id}>
+            <img className={styles.brands__image} src={img} alt={alt} />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- frontend/src/features/home/pages/Homepage.tsx
+++ frontend/src/features/home/pages/Homepage.tsx
@@ -1,5 +1,11 @@
+import Brands from "../components/brands/Brands";
 import Hero from "../components/hero/Hero";
 
 export default function Homepage() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Brands />
+    </>
+  );
 }
