4b35ef3b0a1d343b9aa093a7908f0880cdb60e90
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-10 19:00:18 +0100

    feat: add topbar border

--- frontend/src/features/product/components/productShowcase/ui/productGallery/ProductGallery.tsx
+++ frontend/src/features/product/components/productShowcase/ui/productGallery/ProductGallery.tsx
@@ -1,32 +1,53 @@
+import { useState } from "react";
 import styles from "./ProductGallery.module.css";
 
+const GALLERY_ITEMS = [
+  {
+    id: 1,
+    img: "/src/assets/images/placeholders/product1.svg",
+    alt: "A guy wearing a casual t-shirt",
+  },
+  {
+    id: 2,
+    img: "/src/assets/images/placeholders/product2.svg",
+    alt: "A guy wearing a formal suit",
+  },
+  {
+    id: 3,
+    img: "/src/assets/images/placeholders/product3.svg",
+    alt: "A guy wearing a party dress",
+  },
+];
+
 function ProductGallery() {
+  const [currentImage, setCurrentImage] = useState(1);
+
+  function handleSelectImage(id: number) {
+    if (currentImage === id) return;
+
+    setCurrentImage(id);
+  }
+
+  const bigImage = GALLERY_ITEMS[currentImage - 1];
+
   return (
     <div className={styles.gallery}>
-      <button className={styles.gallery__smallButton}>
-        <img
-          className={styles.gallery__smallImage}
-          src="/src/assets/images/placeholders/product1.png"
-        />
-      </button>
-      <button className={styles.gallery__smallButton}>
-        <img
-          className={styles.gallery__smallImage}
-          src="/src/assets/images/placeholders/product2.png"
-        />
-      </button>
-      <button className={styles.gallery__smallButton}>
-        <img
-          className={styles.gallery__smallImage}
-          src="/src/assets/images/placeholders/product3.png"
-        />
-      </button>
+      {GALLERY_ITEMS.map(({ id, img, alt }) => (
+        <button
+          key={id}
+          onClick={() => handleSelectImage(id)}
+          className={styles.gallery__smallButton}
+        >
+          <img className={styles.gallery__smallImage} src={img} alt={alt} />
+        </button>
+      ))}
 
       <figure className={styles.gallery__big}>
         <button className={styles.gallery__bigButton}>
           <img
             className={styles.gallery__bigImage}
-            src="/src/assets/images/placeholders/productBig.png"
+            src={bigImage.img}
+            alt={bigImage.alt}
           />
         </button>
       </figure>
