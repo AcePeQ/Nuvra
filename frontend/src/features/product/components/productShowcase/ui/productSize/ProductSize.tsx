252bde5a03dd927e6c4b3e72e5ca1798f820dc69
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-11 19:14:38 +0100

    feat: first touch with Product Size component

--- frontend/src/features/product/components/productShowcase/ui/productSize/ProductSize.tsx
+++ frontend/src/features/product/components/productShowcase/ui/productSize/ProductSize.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import styles from "./ProductSize.module.css";
+import { motion } from "framer-motion";
+
+const SIZES = ["Small", "Medium", "Large", "X-Large"];
+
 function ProductSize() {
-  return <div>ProductSize</div>;
+  const [selectedSize, setSelectedSize] = useState<string | null>(null);
+
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>Choose Size</h3>
+      <ul className={styles.sizes}>
+        {SIZES.map((size) => (
+          <li key={size} className={styles.size}>
+            <label htmlFor={size} className={styles.size__label}>
+              <input
+                checked={size === selectedSize}
+                onChange={() => setSelectedSize(size)}
+                className={styles.size__input}
+                id={size}
+                type="radio"
+                name="size"
+                value={size}
+              />
+              {size}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ProductSize;
