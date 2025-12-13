b81f83e2f7d61ab08cdcf98d64b0ffa2c61572c1
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-13 12:42:13 +0100

    feat: add animation in Size buttons for product

--- frontend/src/features/product/components/productShowcase/ui/productSize/ProductSizeItem.tsx
+++ frontend/src/features/product/components/productShowcase/ui/productSize/ProductSizeItem.tsx
@@ -1,5 +1,6 @@
 import { InputRadioListItemChildrenProps } from "../../../../../../shared/ui/inputRadioList/InputRadioList";
 import styles from "./ProductSizeItem.module.css";
+import { motion } from "framer-motion";
 
 function ProductSizeItem({
   item,
@@ -19,7 +20,25 @@ function ProductSizeItem({
         name="size"
         value={item}
       />
-      {item}
+      <div aria-hidden className={styles.firstBG}></div>
+      <span className={styles.text}>{item}</span>
+
+      {item === selected && (
+        <motion.div
+          id="sizeSelected"
+          layoutId="sizeSelected"
+          aria-hidden
+          className={styles.size__selected}
+        >
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring" }}
+          >
+            {item}
+          </motion.span>
+        </motion.div>
+      )}
     </label>
   );
 }
