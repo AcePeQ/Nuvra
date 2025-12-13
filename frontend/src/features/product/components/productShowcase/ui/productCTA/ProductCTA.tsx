3636b395074ef8fad5c5f3100cfc1967cf3d5730
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-13 15:05:04 +0100

    feat: add Counter component
    TODO:
    Add animations

--- frontend/src/features/product/components/productShowcase/ui/productCTA/ProductCTA.tsx
+++ frontend/src/features/product/components/productShowcase/ui/productCTA/ProductCTA.tsx
@@ -1,5 +1,21 @@
+import Button from "../../../../../../shared/ui/button/Button";
+import Counter from "../../../../../../shared/ui/counter/Counter";
+import styles from "./ProductCTA.module.css";
+
 function ProductCTA() {
-  return <div>ProductCTA</div>;
+  return (
+    <div className={styles.productsCTA}>
+      <Counter defaultValue={0} className="product" />
+      <Button
+        type="button"
+        buttonSize="normal"
+        buttonStyle="primary"
+        label="Add to cart"
+      >
+        Add to Cart
+      </Button>
+    </div>
+  );
 }
 
 export default ProductCTA;
