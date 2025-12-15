cfe211db27309e8f61f14f5f5bf512df6023bea7
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-15 19:58:20 +0100

    feat: prepare component for reviews element

---
+++ frontend/src/features/product/components/productShowcase/ui/productReviews/ProductReview.tsx
@@ -0,0 +1,26 @@
+import Rating from "../../../../../../shared/ui/raiting/Raiting";
+import styles from "./ProductReview.module.css";
+
+type ProductReviewProps = {
+  review: any;
+};
+
+function ProductReview({ review }: ProductReviewProps) {
+  return (
+    <li className={styles.review__item}>
+      <article className={styles.review}>
+        <Rating rating={4} />
+        <h3 className={styles.title}>Samantha D.</h3>
+        <p className={styles.description}>
+          I absolutely loved this t-shirt! It fit perfectly and was super soft.
+          The design is very unique and creative, and the colors are perfect for
+          any occasion. As a fellow fashion enthusiast, I can't wait to wear
+          this t-shirt every day. It's a must-have in my wardrobe!
+        </p>
+        <p className={styles.date}>Posted on August 16, 2023</p>
+      </article>
+    </li>
+  );
+}
+
+export default ProductReview;
---
+++ frontend/src/features/product/components/productShowcase/ui/productReviews/ProductReviews.tsx
@@ -0,0 +1,29 @@
+import Button from "../../../../../../shared/ui/button/Button";
+import ProductReview from "./ProductReview";
+import styles from "./ProductReviews.module.css";
+
+const REVIEWS = Array.from({ length: 5 }, (_, index) => index + 1);
+
+function ProductReviews() {
+  return (
+    <section className={styles.reviews__section}>
+      <div>
+        <h2 className={styles.title}>
+          All Reviews <span className={styles.count}>(452)</span>
+        </h2>
+
+        <Button type="button">Write a Review</Button>
+      </div>
+
+      <div className={styles.reviews__container}>
+        <ul className={styles.reviews__list}>
+          {REVIEWS.map((review) => (
+            <ProductReview key={review} review={review} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default ProductReviews;
