68f74915de76f0237c1dbd786b5c58db24bcae3b
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-20 15:48:57 +0100

    feat: add product details element

--- frontend/src/features/product/components/productDetails/ProductDetails.tsx
+++ frontend/src/features/product/components/productDetails/ProductDetails.tsx
@@ -1,7 +1,48 @@
 import styles from "./ProductDetails.module.css";
 
+const PRODUCT_DETAILS = [
+  {
+    title: "Highlights",
+    list: [
+      "Soft, breathable fabric",
+      "Comfortable fit",
+      "Unique design",
+      "High-quality construction",
+    ],
+  },
+  {
+    title: "Material",
+    list: ["100% cotton"],
+  },
+  {
+    title: "Fit",
+    list: ["Regular fit"],
+  },
+  {
+    title: "Delivery",
+    list: ["Standard delivery: 3-5 business days"],
+  },
+];
+
 function ProductDetails() {
-  return <div>ProductDetails</div>;
+  return (
+    <section className={styles.details}>
+      <table className={styles.table}>
+        {PRODUCT_DETAILS.map(({ title, list }) => (
+          <tbody key={title}>
+            <tr>
+              <th>{title}</th>
+            </tr>
+            {list.map((item) => (
+              <tr key={item}>
+                <td>{item}</td>
+              </tr>
+            ))}
+          </tbody>
+        ))}
+      </table>
+    </section>
+  );
 }
 
 export default ProductDetails;
