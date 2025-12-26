15c270d93c24adbc6a0ff956e6045d8670fd6c37
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-26 10:51:45 +0100

    feat: account section padding

--- frontend/src/features/account/pages/account/Account.tsx
+++ frontend/src/features/account/pages/account/Account.tsx
@@ -1,8 +1,9 @@
 import AccountGuest from "../accountGuest/AccountGuest";
+import styles from "./Account.module.css";
 
 function Account() {
   return (
-    <section className="container container-padding">
+    <section className={`container container-padding ${styles.accountSection}`}>
       <AccountGuest />
     </section>
   );
