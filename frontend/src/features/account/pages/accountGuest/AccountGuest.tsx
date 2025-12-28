c3c7e0e564265360a79ac5da521e40c08f5fe200
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-28 14:48:02 +0100

    feat: add additional background for login and register pages

--- frontend/src/features/account/pages/accountGuest/AccountGuest.tsx
+++ frontend/src/features/account/pages/accountGuest/AccountGuest.tsx
@@ -5,19 +5,23 @@ import AnimatedBoxes from "../../../../shared/ui/animatedBoxes/AnimatedBoxes";
 
 export default function AccountGuest() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.form_wrapper}>
-        <h1 className={styles.title}>Welcome,</h1>
-        <p className={styles.subText}>Please enter your details to login</p>
-        <LoginForm />
-        <p className={styles.question}>
-          Don't have an account?{" "}
-          <Link className={styles.link} to=":?=register">
-            Sign up
-          </Link>
-        </p>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.form_wrapper}>
+          <h1 className={styles.title}>Welcome,</h1>
+          <p className={styles.subText}>Please enter your details to login</p>
+          <LoginForm />
+          <p className={styles.question}>
+            Don't have an account?{" "}
+            <Link className={styles.link} to=":?=register">
+              Sign up
+            </Link>
+          </p>
+        </div>
+        <AnimatedBoxes />
       </div>
-      <AnimatedBoxes />
-    </div>
+
+      <div className={styles.treeBG} aria-hidden />
+    </>
   );
 }
