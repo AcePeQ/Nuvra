52a2950f32d9c6bdd3dfbab0163fe44517086e20
AcePeQ <maciej.nojszewski22@gmail.com>
2025-12-29 16:52:06 +0100

    feat: creating submit function for register submitting

--- frontend/src/features/account/components/registerForm/RegisterForm.tsx
+++ frontend/src/features/account/components/registerForm/RegisterForm.tsx
@@ -5,6 +5,13 @@ import Button from "../../../../shared/ui/button/Button";
 import EyeOffIcon from "../../../../shared/icons/EyeOffIcon";
 import EyeOnIcon from "../../../../shared/icons/EyeOnIcon";
 
+interface FormData {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+
 function RegisterForm() {
   const [passwordType, setPasswordType] = useState<string>("password");
 
@@ -13,24 +20,53 @@ function RegisterForm() {
   }
 
   async function registerAction(_, formData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const firstName = formData.get("firstName");
+    const lastName = formData.get("lastName");
+
+    let errors = {};
+
+    if (!email.trim() || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      errors.email = "E-mail field is required or invalid email format";
+    }
+
+    if (
+      !password.trim() ||
+      !email.match(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+      )
+    ) {
+      errors.password =
+        "Password must contain at least 8 characters, including letters, numbers, and special characters";
+    }
+
+    if (!firstName.trim()) {
+      errors.firstName = "First name field is required";
+    }
+
+    if (!lastName.trim()) {
+      errors.lastName = "Last name field is required";
+    }
+
     return {
-      errors: null,
+      errors: {},
     };
   }
 
   const [formState, formAction, isPending] = useActionState(registerAction, {
-    errors: null,
+    errors: {},
   });
 
   return (
     <form action={formAction} className={styles.form}>
-      <FormRow label="E-mail" error={formState.errors?.email}>
+      <FormRow label="E-mail" error={formState.errors.email}>
         <input type="email" placeholder="E-mail address" />
       </FormRow>
-      <FormRow label="First name" error={formState.errors?.firstName}>
+      <FormRow label="First name" error={formState.errors.firstName}>
         <input type="text" placeholder="First name" />
       </FormRow>
-      <FormRow label="Last name" error={formState.errors?.lastName}>
+      <FormRow label="Last name" error={formState.errors.lastName}>
         <input type="text" placeholder="First name" />
       </FormRow>
       <FormRow
@@ -40,9 +76,9 @@ function RegisterForm() {
           passwordType === "password" ? "Show password" : "Hide password"
         }
         iconOnClick={handleChangePasswordType}
-        error={formState.errors?.password}
+        error={formState.errors.password}
       >
-        <input type={passwordType} placeholder="Your password" />
+        <input type={passwordType} placeholder="Password" />
       </FormRow>
 
       <Button
