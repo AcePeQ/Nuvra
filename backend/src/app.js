2e8f7af701add022be8402d133a22385ec1bff52
AcePeQ <maciej.nojszewski22@gmail.com>
2026-01-04 12:52:58 +0100

    feat: add cors configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import cors from "cors";
 export const app = express();
+
+const corsOptions = {
+  origin: process.env.ENVIRONMENT,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.static("../public"));
+
 app.use(express.json());
