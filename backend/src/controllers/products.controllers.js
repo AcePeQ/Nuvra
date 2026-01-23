a96a9116db8f5ce98b74a75b77046476caec98dd
AcePeQ <maciej.nojszewski22@gmail.com>
2026-01-23 15:09:31 +0100

    feat: create DB Pools for showcase products

--- backend/src/controllers/products.controllers.js
+++ backend/src/controllers/products.controllers.js
@@ -7,11 +7,23 @@ export async function allProducts(req, res, next) {
     if (!products) {
       return next({ status: 404, message: "Could not find products" });
     }
-    console.log(products);
 
     return res.status(200).json({ products });
   } catch (error) {
     console.error("Error in allProducts controller: ", error);
+    next(error);
+  }
+}
+
+export async function allShowcaseProducts(req, res, next) {
+  try {
+    const products = await getAllProducts();
+
+    if (!products) {
+      return next({ status: 404, message: "Could not find products" });
+    }
+  } catch (error) {
+    console.error("Error in allShowcaseProducts controller: ", error);
     next(error);
   }
 }
--- backend/src/services/products.service.js
+++ backend/src/services/products.service.js
@@ -1,6 +1,27 @@
 import pool from "../db/db.js";
 
 export async function getAllProducts() {
-  const result = await pool.query("SELECT * from products");
+  const result = await pool.query("SELECT * FROM products");
+  return result.rows;
+}
+
+export async function getNewArrivalProducts() {
+  const result = await pool.query(
+    "SELECT * FROM products WHERE is_new_arrival=true ",
+  );
+  return result.rows;
+}
+
+export async function getTopSaleProducts() {
+  const result = await pool.query(
+    "SELECT * FROM products WHERE is_featured=true",
+  );
+  return result.rows;
+}
+
+export async function getOnSaleProducts() {
+  const result = await pool.query(
+    "SELECT * FROM products WHERE dicsount_perecent > 0",
+  );
   return result.rows;
 }
