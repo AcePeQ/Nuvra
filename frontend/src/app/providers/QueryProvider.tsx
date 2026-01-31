1fb9a188efa92d19bb2ef9025795d8c5c8d375b5
AcePeQ <maciej.nojszewski22@gmail.com>
2026-01-31 14:05:53 +0100

    feat: set default options for react query - queries

--- frontend/src/app/providers/QueryProvider.tsx
+++ frontend/src/app/providers/QueryProvider.tsx
@@ -1,7 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 15,
+      gcTime: 1000 * 60 * 15,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function QueryProvider({
   children,
