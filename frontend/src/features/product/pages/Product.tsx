b034f991821a1e92bcaec5740b2e1bd7e5e3910c
AcePeQ <maciej.nojszewski22@gmail.com>
2026-02-04 20:30:40 +0100

    feat: delete placeholders products

--- frontend/src/features/product/pages/Product.tsx
+++ frontend/src/features/product/pages/Product.tsx
@@ -7,54 +7,6 @@ import styles from "./Product.module.css";
 import ErrorFull from "../../../shared/ui/errors/errorFull/ErrorFull";
 import LoaderFull from "../../../shared/ui/loaders/loaderFull/LoaderFull";
 
-const PLACEHOLDER = [
-  {
-    id: 1,
-    name: "T-shirt with Tape Details",
-    img: "/src/assets/images/placeholders/searchPlaceholder2.png",
-    rating: 3,
-    currentPrice: 240,
-    originalPrice: 260,
-    discount: 20,
-  },
-  {
-    id: 2,
-    name: "T-shirt with Tape Details",
-    img: "/src/assets/images/placeholders/searchPlaceholder2.png",
-    rating: 3.5,
-    currentPrice: 240,
-    originalPrice: 260,
-    discount: 20,
-  },
-  {
-    id: 3,
-    name: "T-shirt with Tape Details",
-    img: "/src/assets/images/placeholders/searchPlaceholder2.png",
-    rating: 5,
-    currentPrice: 240,
-    originalPrice: 260,
-    discount: 20,
-  },
-  {
-    id: 4,
-    name: "T-shirt with Tape Details",
-    img: "/src/assets/images/placeholders/searchPlaceholder2.png",
-    rating: 4,
-    currentPrice: 240,
-    originalPrice: 260,
-    discount: 20,
-  },
-  {
-    id: 5,
-    name: "T-shirt with Tape Details",
-    img: "/src/assets/images/placeholders/searchPlaceholder2.png",
-    rating: 4,
-    currentPrice: 240,
-    originalPrice: 260,
-    discount: 20,
-  },
-];
-
 function Product() {
   const { productName } = useParams();
 
