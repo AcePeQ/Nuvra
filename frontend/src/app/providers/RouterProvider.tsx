5f19d809bef8e5bfcf8ebe33e7d237ee86a74d4d
AcePeQ <maciej.nojszewski22@gmail.com>
2026-02-26 18:14:34 +0100

    feat: finish AccountGuest component that manage login and register pages

--- frontend/src/app/providers/RouterProvider.tsx
+++ frontend/src/app/providers/RouterProvider.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         element: <Homepage />,
       },
       {
-        path: "/shop",
+        path: "shop",
         element: <Shop />,
       },
       {
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <Cart />,
       },
       {
-        path: "account",
+        path: "account/:mode?",
         element: <Account />,
       },
 
--- frontend/src/features/account/pages/accountGuest/AccountGuest.tsx
+++ frontend/src/features/account/pages/accountGuest/AccountGuest.tsx
@@ -1,21 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import LoginForm from "../../components/loginForm/LoginForm";
 import styles from "./AccountGuest.module.css";
 import AnimatedBoxes from "../../../../shared/ui/animatedBoxes/AnimatedBoxes";
 import RegisterForm from "../../components/registerForm/RegisterForm";
 
+
+const modes = {
+  register: {
+    greeting: "Welcome,",
+    subText: "Please enter your details to register.",
+    question: "Do you have account?",
+    linkTo: "login",
+    linkText: "Sign in"
+  },
+  login: {
+    greeting: "Welcome again!",
+    subText: "Please enter your details to enter your account.",
+    question: "Don't have an account?",
+    linkTo: "register",
+    linkText: "Sign up"
+  }
+}
+
 export default function AccountGuest() {
+  const { mode } = useParams();
+
+  const isRegisterPage = mode === "register"
+  const { greeting, subText, question, linkTo, linkText } = modes[isRegisterPage ? "register" : "login"];
+
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.form_wrapper}>
-          <h1 className={styles.title}>Welcome,</h1>
-          <p className={styles.subText}>Please enter your details to login</p>
-          <RegisterForm />
+          <h1 className={styles.title}>{greeting}</h1>
+          <p className={styles.subText}>{subText}</p>
+          {isRegisterPage ?
+            <RegisterForm />
+            :
+            <LoginForm />
+          }
           <p className={styles.question}>
-            Don't have an account?{" "}
-            <Link className={styles.link} to=":?=register">
-              Sign up
+            {question}{" "}
+            <Link className={styles.link} to={`/account/${linkTo}`}>
+              {linkText}
             </Link>
           </p>
         </div>
