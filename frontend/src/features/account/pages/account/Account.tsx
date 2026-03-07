9904397059bd4f3de6e8398d4a6d0fa300107b98
AcePeQ <maciej.nojszewski22@gmail.com>
2026-03-07 09:59:57 +0100

    feat: update users service, add a new column for users to check if it's the first user's order

--- frontend/src/features/account/pages/account/Account.tsx
+++ frontend/src/features/account/pages/account/Account.tsx
@@ -1,10 +1,16 @@
+import { useIsLoggedIn } from "../../stores/userStore";
 import AccountGuest from "../accountGuest/AccountGuest";
 import styles from "./Account.module.css";
 
 function Account() {
+  const isLoggedIn = useIsLoggedIn();
+
   return (
     <section className={`container container-padding ${styles.accountSection}`}>
-      <AccountGuest />
+      {isLoggedIn ? <div>Test</div>
+        :
+        <AccountGuest />
+      }
     </section>
   );
 }
