1123c00477c89706872d0fa23d03c3a5974b736d
AcePeQ <maciej.nojszewski22@gmail.com>
2026-04-03 15:32:10 +0200

    feat: add rating select component

--- frontend/src/features/product/components/productReviewForm/ProductReviewForm.tsx
+++ frontend/src/features/product/components/productReviewForm/ProductReviewForm.tsx
@@ -1,6 +1,9 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import styles from './ProductReviewForm.module.css';
 import FormRow from '../../../../shared/ui/formRow/FormRow';
+import Button from '../../../../shared/ui/button/Button';
+import { useModalContext } from '../../../../shared/ui/modal/Modal';
+import RatingSelect from '../../../../shared/ui/ratingSelect/RatingSelect';
 
 type Inputs = {
   message: string;
@@ -8,6 +11,8 @@ type Inputs = {
 }
 
 function ProductReviewForm() {
+  const { toggleModal } = useModalContext()
+
   const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
     defaultValues: {}
   })
@@ -19,13 +24,25 @@ function ProductReviewForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-      <FormRow label='Message' error={errors.message?.message}>
-        <textarea id='message'></textarea>
-      </FormRow>
+      <div className={styles.rows}>
+        <FormRow label='Message' error={errors.message?.message}>
+          <textarea placeholder='Write your review here... (max 250 characters)' id='message' rows={10} {...register('message', {
+            maxLength: {
+              value: 250,
+              message: "Your message must have less than 250 characters"
+            }
+          })}></textarea>
+        </FormRow>
 
-      <FormRow label='Rating' error={errors.rate?.message}>
-        <input />
-      </FormRow>
+        <FormRow label='Rating' error={errors.rate?.message}>
+          <RatingSelect />
+        </FormRow>
+      </div>
+
+      <div className={styles.buttonsWrapper}>
+        <Button type="submit" buttonSize='small'>Add Review</Button>
+        <Button type='button' buttonSize='small' onClick={() => toggleModal(false)}>Close</Button>
+      </div>
     </form>
   )
 }
